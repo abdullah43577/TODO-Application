bdf3ec6b7a81597b4baaaffb13b8dd7749cdb8ec
abdullah43577 <100965547+abdullah43577@users.noreply.github.com>
2022-09-28 21:39:43 +0100

    first deployment

---
+++ index.js
@@ -0,0 +1,59 @@
+let myLists = [];
+const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+const check = document.querySelector("#check-el");
+const header = document.querySelector("#header-el");
+const trash = document.querySelector("#trashcan");
+// getting the item from localStorage and passing in the parse method to parse it back to an object or array
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLists"));
+
+// this evaluates to a true or false situation to determine whether the code runs or not. this will make the inputs not able to vanish on refresh of the page
+if (leadsFromLocalStorage) {
+  myLists = leadsFromLocalStorage;
+  render(myLists);
+}
+
+// to get the current date
+const today = new Date().toLocaleDateString();
+
+header.innerHTML = `To-Do Lists For Today ${today}`;
+
+trash.addEventListener("dblclick", function () {
+  let temporaryArray = [];
+  for (let i = 0; i < myLists.length; i++) {
+    if (!document.querySelector(`#check${i}`).checked) {
+      temporaryArray.push(myLists[i]);
+    }
+    console.log(temporaryArray);
+  }
+  myLists = temporaryArray;
+  //live reload function
+  location.reload();
+  console.log(myLists);
+  localStorage.setItem("myLists", JSON.stringify(myLists));
+});
+
+function render(lists) {
+  let listItems = "";
+  for (let i = 0; i < lists.length; i++) {
+    listItems += `<input type="checkbox" id="check${i}" name="list${i}" class="checks"/>
+                       <label for="check${i}">
+                            ${lists[i]}
+                        </label>
+                        <br>`;
+  }
+  console.log(listItems);
+  //we'll later need to modify this to make the first item in the div permanent
+  check.innerHTML = listItems;
+}
+
+inputBtn.addEventListener("click", function () {
+  //pushing the value in the input field to the array myLists
+  myLists.push(inputEl.value);
+  //setting inputEl.value to an empty string clears out the input field when the content has been added to the array
+  inputEl.value = "";
+  // saving the myLists array to localStorage (localStorages only accepts strings as input not objects or arrays, hence the need for the JSON.stringify)
+  localStorage.setItem("myLists", JSON.stringify(myLists));
+  console.log(myLists);
+  render(myLists);
+});
